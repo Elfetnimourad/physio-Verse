8fedd4e14d7005d8badbfc810aceea2a4d44a5b1
Elfetnimourad <moradtao2000@gmail.com>
2025-09-25 12:29:27 +0100

    fetching data

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,27 +2,58 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
-
+import XIcon from '@mui/icons-material/X';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
 function Footer() {
   return (
       <>
 
        <Divider style={{border:'1px solid white'}}></Divider>
-     <Box sx={{ gap: 1 }}>
+     <Box sx={{ gap: 1 }} style={{padding:5}}>
       <Grid container spacing={2}>
         <Grid size={6}>
          <h2>PhysioVerse.</h2>
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum unde quaerat eveniet cumque accusamus atque qui error quo enim fugiat?</p>
         </Grid>
-        <Grid size={2}>
-         size=4
+        <Grid size={3}>
+          <h3>Links.</h3>
+         <div className="d-flex justify-content-center mb-4 row">
+          <a href="#about" className="text-light me-4 text-decoration-none">
+            About
+          </a>
+          <a href="#theories" className="text-light me-4 text-decoration-none">
+            Theories
+          </a>
+          <a href="#research" className="text-light me-4 text-decoration-none">
+            Research
+          </a>
+          <a href="#contact" className="text-light me-4 text-decoration-none">
+            Contact
+          </a>
+        </div>
         </Grid>
-        <Grid size={2}>
-         size=4
+        <Grid size={3}>
+          <h3>Socials.</h3>
+           <div className="mb-3 row">
+          <a href="https://twitter.com" className="text-light me-3 fs-4">
+            <XIcon/>
+          </a>
+          <a href="https://facebook.com" className="text-light me-3 fs-4">
+           
+             <FacebookIcon/>
+          </a>
+          <a href="https://github.com" className="text-light fs-4">
+                         <GitHubIcon/>
+
+          </a>
+        </div>
         </Grid>
-        <Grid size={2}>
-         size=8
-        </Grid>
+        
       </Grid>
+       <p className="small mb-0 text-center">
+          © {new Date().getFullYear()} Science Explorer | Made with curiosity 🪐
+        </p>
     </Box>
         </>
 
--- src/component/Studies.tsx
+++ src/component/Studies.tsx
@@ -1,21 +1,73 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import TextField from '@mui/material/TextField';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import IconButton from '@mui/material/IconButton';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+
 
 function Studies() {
+const [topic,setTopic] = useState("");
+const [items,setItems] = useState([])
+ async function fetchData(topic){
+const res = await fetch(`https://api.crossref.org/works?query=${topic}&rows=10`);
+  const data = res.json().then(d=>d.message.items);
+  console.log(data)
+  setItems(...data)
+   }
+console.log(topic)
+//   useEffect(() => {
+//   async function fetchData(){
+// const res = await fetch(`https://api.crossref.org/works?query`);
+//   const data = res.json();
+//   console.log(data)
+//    }
+
+//    fetchData()
+  
+//   }, [])
   return (
     <div className='d-flex row justify-content-center' >
           <TextField  label="fullWidth" id="fullWidth" className="w-50" style={{width:"40%"}} />
 
     <div className='d-flex column justify-content-evenly' style={{width:"60%"}}>
-        <nav>All</nav>
-        <nav>blz</nav>
-        <nav>clz </nav>
-        <nav>dvd</nav>
+        <nav >All</nav>
+        <nav onClick={()=>fetchData("relativity")}>relativity</nav>
+        <nav onClick={()=>fetchData("quantum")}>quantum </nav>
+        <nav onClick={()=>fetchData("black hole")}>black hole</nav>
         <nav>csz</nav>
       </div>
     
       <div>
-            here  whare you see the studies.
+           {/* <ImageListItem key={item.img} >
+            <img
+             
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              sx={{
+                background:
+                  'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+                  'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+              }}
+              title={item.title}
+              position="top"
+              actionIcon={
+                <IconButton
+                  sx={{ color: 'white' }}
+                  aria-label={`star ${item.title}`}
+                >
+                  <StarBorderIcon />
+                </IconButton>
+              }
+              actionPosition="left"
+            />
+          </ImageListItem>
+       */}
+  
+
         </div>
 
     </div>
